e5cfb3bec3289f2905c6a716fc43cc0c996d7773
Carlos Carvalho <ccarvalho@expertisesolutions.com.br>
2014-09-12 16:29:16 -0300

    esql_model: Basic implementation of constructor, destructor, load and unload.

--- src/lib/esql_model.c
+++ src/lib/esql_model.c
@@ -13,15 +13,72 @@
 #define MY_CLASS ESQL_MODEL_CLASS
 #define MY_CLASS_NAME "Esql_Model"
 
-/**
- * 'Implements' section
- */
-void _esql_model_eo_base_constructor(Eo *obj EINA_UNUSED, Esql_Model_Data *pd EINA_UNUSED)
+static void
+_load_set(Esql_Model_Data *priv, Emodel_Load load)
 {
+   if ((priv->load.status & (EMODEL_LOAD_STATUS_LOADED | EMODEL_LOAD_STATUS_LOADING)) &&
+                        (load.status & (EMODEL_LOAD_STATUS_LOADED | EMODEL_LOAD_STATUS_LOADING)))
+     {
+        load.status = priv->load.status | load.status;
+        switch (load.status)
+          {
+           case EMODEL_LOAD_STATUS_LOADED_PROPERTIES:
+             load.status &= ~EMODEL_LOAD_STATUS_LOADING_PROPERTIES;
+             break;
+           case EMODEL_LOAD_STATUS_LOADING_PROPERTIES:
+             load.status &= ~EMODEL_LOAD_STATUS_LOADED_PROPERTIES;
+             break;
+           case EMODEL_LOAD_STATUS_LOADED_CHILDREN:
+             load.status &= ~EMODEL_LOAD_STATUS_LOADING_CHILDREN;
+             break;
+           case EMODEL_LOAD_STATUS_LOADING_CHILDREN:
+             load.status &= ~EMODEL_LOAD_STATUS_LOADED_CHILDREN;
+             break;
+           default: break;
+          }
+     }
+
+   if (priv->load.status != load.status)
+     {
+         priv->load.status = load.status;
+         eo_do(priv->obj, eo_event_callback_call(EMODEL_EVENT_LOAD_STATUS, &load));
+     }
 }
 
-void _esql_model_eo_base_destructor(Eo *obj EINA_UNUSED, Esql_Model_Data *pd EINA_UNUSED)
+/**
+ * Custom constructor
+ */
+void _esql_model_constructor(Eo *obj, Esql_Model_Data *pd,
+                                const char *addr, const char *user, const char *password)
 {
+   Emodel_Load load;
+   Esql_Type type = ESQL_TYPE_SQLITE;
+
+   EINA_SAFETY_ON_NULL_RETURN(addr);
+   EINA_SAFETY_ON_NULL_RETURN(user);
+   EINA_SAFETY_ON_NULL_RETURN(password);
+
+   eo_do_super(obj, MY_CLASS, eo_constructor());
+
+   pd->obj = obj;
+   pd->e = esql_new(type);
+   EINA_SAFETY_ON_NULL_RETURN(pd->e);
+
+   pd->backend_type = type;
+   pd->conn.addr = addr;
+   pd->conn.user = user;
+   pd->conn.password = password;
+   pd->load.status = EMODEL_LOAD_STATUS_UNLOADED;
+
+   load.status = EMODEL_LOAD_STATUS_UNLOADED;
+   _load_set(pd, load);
+}
+
+void _esql_model_eo_base_destructor(Eo *obj, Esql_Model_Data *pd EINA_UNUSED)
+{
+   EINA_SAFETY_ON_NULL_RETURN(pd->e);
+   esql_free(pd->e);
+   eo_do_super(obj, MY_CLASS, eo_destructor());
 }
 
 Emodel_Load_Status _esql_model_emodel_properties_list_get(Eo *obj EINA_UNUSED,
@@ -29,7 +86,6 @@ Emodel_Load_Status _esql_model_emodel_properties_list_get(Eo *obj EINA_UNUSED,
 {
    return EMODEL_LOAD_STATUS_ERROR;
 }
-
 
 void _esql_model_emodel_properties_load(Eo *obj EINA_UNUSED, Esql_Model_Data *pd EINA_UNUSED)
 {
@@ -41,25 +97,39 @@ Emodel_Load_Status _esql_model_emodel_property_set(Eo *obj EINA_UNUSED,
    return EMODEL_LOAD_STATUS_ERROR;
 }
 
-
 Emodel_Load_Status _esql_model_emodel_property_get(Eo *obj EINA_UNUSED,
                                 Esql_Model_Data *pd EINA_UNUSED, const char * property EINA_UNUSED, Eina_Value *value EINA_UNUSED)
 {
    return EMODEL_LOAD_STATUS_ERROR;
 }
 
-
-void _esql_model_emodel_load(Eo *obj EINA_UNUSED, Esql_Model_Data *pd EINA_UNUSED)
+void _esql_model_emodel_load(Eo *obj EINA_UNUSED, Esql_Model_Data *pd)
 {
+   Emodel_Load load;
+
+   Eina_Bool ret = esql_connect(pd->e, pd->conn.addr, pd->conn.user, pd->conn.password);
+   EINA_SAFETY_ON_FALSE_RETURN(ret);
+   load.status = EMODEL_LOAD_STATUS_LOADED_PROPERTIES;
+
+   load.status = EMODEL_LOAD_STATUS_LOADED;
+   _load_set(pd, load);
 }
 
-Emodel_Load_Status _esql_model_emodel_load_status_get(Eo *obj EINA_UNUSED, Esql_Model_Data *pd EINA_UNUSED)
+Emodel_Load_Status _esql_model_emodel_load_status_get(Eo *obj EINA_UNUSED, Esql_Model_Data *pd)
 {
-   return EMODEL_LOAD_STATUS_ERROR;
+   return pd->load.status;
 }
 
-void _esql_model_emodel_unload(Eo *obj EINA_UNUSED, Esql_Model_Data *pd EINA_UNUSED)
+void _esql_model_emodel_unload(Eo *obj EINA_UNUSED, Esql_Model_Data *pd)
 {
+   Emodel_Load load;
+   Eina_Bool ret = esql_isconnected(pd->e);
+
+   EINA_SAFETY_ON_FALSE_RETURN(ret);
+
+   esql_disconnect(pd->e);
+   load.status = EMODEL_LOAD_STATUS_UNLOADED;
+   _load_set(pd, load);
 }
 
 Eo * _esql_model_emodel_child_add(Eo *obj EINA_UNUSED, Esql_Model_Data *pd EINA_UNUSED)
--- src/lib/esql_model_private.h
+++ src/lib/esql_model_private.h
@@ -5,11 +5,23 @@
 
 typedef struct _Esql_Model_Data Esql_Model_Data;
 
+typedef struct _Esql_Model_Connection_Data Esql_Model_Connection_Data;
+
+struct _Esql_Model_Connection_Data
+{
+   const char *addr;
+   const char *user;
+   const char *password;
+};
+
 struct _Esql_Model_Data
 {
+   Eo *obj;
    Esql *e; /**< Opaque base Esskyuehl object for connecting to servers */
    Esql_Type backend_type; /**< MySQL, SQLite, Postgress... */
    Esql_Query_Id query_id; /**< current query id */
+   Esql_Model_Connection_Data conn;
+   Emodel_Load load;
 };
 
 #endif
