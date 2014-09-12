1771f348ad707dbfa1d97b61b72c89ce228b6c47
Carlos Carvalho <ccarvalho@expertisesolutions.com.br>
2014-09-12 15:13:59 -0300

    Added 'implements' section in eo file plus some data in private data structure.

--- src/lib/esql_model.c
+++ src/lib/esql_model.c
@@ -10,5 +10,82 @@
 
 #include "esql_model_private.h"
 
+#define MY_CLASS ESQL_MODEL_CLASS
+#define MY_CLASS_NAME "Esql_Model"
+
+/**
+ * 'Implements' section
+ */
+void _esql_model_eo_base_constructor(Eo *obj EINA_UNUSED, Esql_Model_Data *pd EINA_UNUSED)
+{
+}
+
+void _esql_model_eo_base_destructor(Eo *obj EINA_UNUSED, Esql_Model_Data *pd EINA_UNUSED)
+{
+}
+
+Emodel_Load_Status _esql_model_emodel_properties_list_get(Eo *obj EINA_UNUSED,
+                                Esql_Model_Data *pd EINA_UNUSED, const Eina_List **properties_list EINA_UNUSED)
+{
+   return EMODEL_LOAD_STATUS_ERROR;
+}
+
+
+void _esql_model_emodel_properties_load(Eo *obj EINA_UNUSED, Esql_Model_Data *pd EINA_UNUSED)
+{
+}
+
+Emodel_Load_Status _esql_model_emodel_property_set(Eo *obj EINA_UNUSED,
+                                Esql_Model_Data *pd EINA_UNUSED, const char * property EINA_UNUSED, Eina_Value value EINA_UNUSED)
+{
+   return EMODEL_LOAD_STATUS_ERROR;
+}
+
+
+Emodel_Load_Status _esql_model_emodel_property_get(Eo *obj EINA_UNUSED,
+                                Esql_Model_Data *pd EINA_UNUSED, const char * property EINA_UNUSED, Eina_Value *value EINA_UNUSED)
+{
+   return EMODEL_LOAD_STATUS_ERROR;
+}
+
+
+void _esql_model_emodel_load(Eo *obj EINA_UNUSED, Esql_Model_Data *pd EINA_UNUSED)
+{
+}
+
+Emodel_Load_Status _esql_model_emodel_load_status_get(Eo *obj EINA_UNUSED, Esql_Model_Data *pd EINA_UNUSED)
+{
+   return EMODEL_LOAD_STATUS_ERROR;
+}
+
+void _esql_model_emodel_unload(Eo *obj EINA_UNUSED, Esql_Model_Data *pd EINA_UNUSED)
+{
+}
+
+Eo * _esql_model_emodel_child_add(Eo *obj EINA_UNUSED, Esql_Model_Data *pd EINA_UNUSED)
+{
+   return NULL;
+}
+
+Emodel_Load_Status _esql_model_emodel_child_del(Eo *obj EINA_UNUSED, Esql_Model_Data *pd EINA_UNUSED, Eo *child EINA_UNUSED)
+{
+   return EMODEL_LOAD_STATUS_ERROR;
+}
+
+Emodel_Load_Status _esql_model_emodel_children_slice_get(Eo *obj EINA_UNUSED, Esql_Model_Data *pd EINA_UNUSED,
+                                unsigned start EINA_UNUSED, unsigned count EINA_UNUSED, Eina_Accessor **children_accessor EINA_UNUSED)
+{
+   return EMODEL_LOAD_STATUS_ERROR;
+}
+
+Emodel_Load_Status _esql_model_emodel_children_count_get(Eo *obj EINA_UNUSED,
+                                Esql_Model_Data *pd EINA_UNUSED, unsigned *children_count EINA_UNUSED)
+{
+   return EMODEL_LOAD_STATUS_ERROR;
+}
+
+void _esql_model_emodel_children_load(Eo *obj EINA_UNUSED, Esql_Model_Data *pd EINA_UNUSED)
+{
+}
 
 #include "esql_model.eo.c"
--- src/lib/esql_model_private.h
+++ src/lib/esql_model_private.h
@@ -1,10 +1,15 @@
 #ifndef _ESQL_MODEL_PRIVATE_H
 #define _ESQL_MODEL_PRIVATE_H
 
+#include <Esskyuehl.h>
+
 typedef struct _Esql_Model_Data Esql_Model_Data;
 
 struct _Esql_Model_Data
 {
+   Esql *e; /**< Opaque base Esskyuehl object for connecting to servers */
+   Esql_Type backend_type; /**< MySQL, SQLite, Postgress... */
+   Esql_Query_Id query_id; /**< current query id */
 };
 
 #endif
